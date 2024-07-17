ee6fef08248f8947301f7c0c64ca5c1019ad441a
jb520 <jnavarro1995@gmail.com>
2024-07-17 11:11:49 -0500

    create like_policy

--- app/policies/like_policy.rb
+++ app/policies/like_policy.rb
@@ -1,3 +1,16 @@
 class LikePolicy
   
+  def initialize(user, like)
+    @user = user
+    @like = like
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    user == like.fan
+  end
+
 end
